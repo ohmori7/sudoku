8edc94138f83505284a5cd5db6c786e9b31254a4
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-07-08 17:19:12 +0900

    separate matrix/sudoku class into another file.

---
+++ main.php
@@ -0,0 +1,36 @@
+<?php
+require_once('sudoku.php');
+
+define('N',		NULL);
+define('A',		0xa);
+define('B',		0xb);
+define('C',		0xc);
+define('D',		0xd);
+define('E',		0xe);
+define('F',		0xf);
+
+$init = array(
+	array(N, 4, N, E,	N, 3, N, 0,	6, N, N, F,	N, N, C, N),
+	array(B, N, N, N,	N, N, 5, D,	N, A, N, N,	4, E, N, 7),
+	array(N, C, N, D,	9, N, 4, N,	3, 7, N, N,	N, N, 0, N),
+	array(8, N, 0, N,	N, N, 2, N,	D, N, 9, N,	3, N, N, 1),
+
+	array(N, 5, N, N,	N, N, D, N,	B, F, N, 4,	N, 9, 1, 6),
+	array(6, N, 3, B,	5, 7, N, F,	N, N, N, A,	N, N, N, N),
+	array(N, N, F, 7,	N, 4, N, N,	N, N, 5, 8,	C, N, 3, N),
+	array(N, A, N, N,	3, N, 0, N,	N, N, 7, N,	E, N, F, N),
+
+	array(1, N, N, N,	2, N, A, 6,	N, E, N, B,	F, N, N, N),
+	array(N, N, 8, C,	N, F, N, N,	0, 6, N, N,	N, B, N, 9),
+	array(4, N, E, N,	7, N, N, N,	5, N, N, N,	N, 0, N, 3),
+	array(7, B, N, N,	0, N, 1, N,	N, 9, C, N,	6, N, 4, N),
+
+	array(3, 6, N, 9,	N, N, N, B,	N, N, D, E,	N, 4, 5, N),
+	array(N, N, B, N,	D, 6, N, N,	7, N, N, C,	9, N, N, 2),
+	array(N, 8, N, 0,	N, 9, N, N,	A, N, 6, N,	B, N, N, F),
+	array(N, N, C, N,	N, 2, F, 3,	N, N, N, 9,	N, 7, N, N),
+);
+
+$m = new Matrix($init);
+$m->dump();
+?>
